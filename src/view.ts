8ce5820aeac2c540a90c87278bc3c8e0f7be9c82
nmwei <254060001@qq.com>
2024-11-28 17:10:21 +0800

    支持撤销、还原、回车、光标移动

--- src/view.ts
+++ src/view.ts
@@ -3,6 +3,13 @@ import { EditorView } from 'prosemirror-view'
 import { EditorState } from 'prosemirror-state'
 import { Schema } from 'prosemirror-model';
 
+import { keymap } from 'prosemirror-keymap'
+// baseKeymap 定义了对于很多基础按键按下后的功能，例如回车换行，删除键等。
+import { baseKeymap } from 'prosemirror-commands'
+// history 是操作历史，提供了对保存操作历史以及恢复等功能，undo，redo 函数对应为进行 undo 操作与 redo 操作，恢复历史数据
+import { history, undo, redo } from 'prosemirror-history'
+
+
 const schema = new Schema({
     nodes: {
         doc: { content: "block+" },
@@ -21,6 +28,14 @@ const state = EditorState.create({
         schema.nodes.paragraph.create({}, [schema.text("Hello, world!")])
     ]),
     schema,
+    plugins: [
+        // 将基础按键绑定到对应的功能上，例如回车换行，删除键等。
+        keymap(baseKeymap),
+        // 提供输入历史栈功能
+        history(),
+        // 将组合按键 ctrl/cmd + z, ctrl/cmd + y 分别绑定到 undo, redo 功能上
+        keymap({"Mod-z": undo, "Mod-y": redo}),
+    ]
 })
 
 const editorView = new EditorView(document.querySelector("#editor"), {
