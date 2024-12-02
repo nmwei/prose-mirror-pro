97a8efacdb65044e4a07eadb937c14389a63f1aa
nmwei <254060001@qq.com>
2024-12-02 10:43:43 +0800

    定义 Node 到 DOM 的转换规则

--- src/schema.ts
+++ src/schema.ts
@@ -3,13 +3,21 @@ import { Schema } from "prosemirror-model"
 export const schema = new Schema({
     nodes: {
         doc: {
-            content: "block_title+"
+            content: "title+"
         },
         block_title: {
-            content: "block",
+            content: "block+",
             group: "title",
             inline: false,
-            toDOM: () => ["div", 0]
+            //toDOM: () => ["div", { "class": "block_title" }, 0]
+            toDOM(){
+                const blockTile = document.createElement('div');
+                blockTile.classList.add('block_tile');
+                return {
+                    dom: blockTile,
+                    contentDOM: blockTile
+                }
+            }
         },
         paragraph: {
             group: "block",
